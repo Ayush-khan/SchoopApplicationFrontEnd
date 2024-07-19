36eaf71c37f9bfb8fb14f4f7367c0f93948886e0
Ayush-Khan <ayushkhana230@gmail.com>
2024-07-19 07:38:21 -0700

    .

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 // import NavBar from "./NavBar";
-// This is a function of the mainLayout
+// This is a function of the mainLayout components where all routes render
 
 function MainLayout() {
   return (

ec19ceb2a83aa97150a2ed66e359f1e7a9cf5021
Ayush-Khan <ayushkhana230@gmail.com>
2024-07-19 07:36:01 -0700

    .

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 // import NavBar from "./NavBar";
-// This
+// This is
 function MainLayout() {
   return (
     <div>
